b4d15a1acaca68f55ecbd56c152f51dc57e84100
Siraj Ahmadzai <siraj_ahmadzai@hotmail.com>
2019-08-09 01:22:58 -0400

    Upload graphbook project to GitHub

---
+++ webpack.client.build.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const buildDirectory = 'dist';
+const outputDirectory = buildDirectory + '/client';
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    mode: 'development',
+    entry: './src/client/index.js',
+    output: {
+        path: path.join(__dirname, outputDirectory),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                presets: ['@babel/preset-react']
+               }
+            }
+        },{  test: /\.css$/,  use: [{ loader: MiniCssExtractPlugin.loader,    options: {      publicPath: '../'    } }, 'css-loader'],}]
+    },
+    
+    plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({
+        template: './public/index.html'
+    }), new MiniCssExtractPlugin({
+  filename: 'bundle.css',
+})]
+};
