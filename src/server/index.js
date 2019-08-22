201be70f4dbe8bc246acf1fafa189582de25202f
Siraj Ahmadzai <siraj_ahmadzai@hotmail.com>
2019-08-22 12:00:35 -0400

    Add setup for the back end

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,36 @@
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import compress from 'compression'
+import services from './services';
 
 const app = express();
 
-app.get('*', (req, res) => res.send('Hello World!'));app.listen(8000, () => console.log('Listening on port 8000!'));
+app.get('*', (req, res) => res.send('Hello World!'));
+app.listen(8000, () => console.log('Listening on port 8000!'));
 
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    styleSrc: ["'self'", "'unsafe-inline'"],
+    imgSrc: ["'self'", "data:", "*.amazonaws.com"]
+  }
+}));
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
 
+app.use(compress());
+
+app.use(cors());
+
+const serviceNames = Object.keys(services);
+
+for (let i = 0; i < serviceNames.length; i += 1) {
+  const name = serviceNames[i];
+  if (name === 'graphql') {
+    services[name].applyMiddleware({ app });
+  } else {
+    app.use(`/${name}`, services[name]);
+  }
+}
---
+++ src/server/services/graphql/resolvers.js
@@ -0,0 +1,22 @@
+const resolvers = {
+	RootQuery: {
+		posts(root, args, context){
+			return [];		
+			},
+  
+    },
+      RootMutation: {
+    	addPost(root, { post, user }, context){
+    	const postObject = {
+    	...post,
+    	user,
+    	id: posts.length + 1,
+    	};
+    	posts.push(postObject);
+    	return postObject;
+	    },
+},		
+	};
+
+
+export default resolvers;	
